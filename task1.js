8dce340995c65b2049c5080992f6c031398ac827
Yerzhanov <dearhauer@gmail.com>
2023-04-14 17:16:14 +0600

    Первое задание с JSON API

---
+++ task1.js
@@ -0,0 +1,79 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+
+app.get('/', (req, res) =>{
+    res.send("Задание 1");
+});
+
+app.get('/add', (req, res) => {
+    let n1 = req.body.A;
+    let n2 = req.body.B;
+    let sum = n1 + n2;
+    res.send(`Результат: ${sum}`);
+});
+
+app.get('/subtract', (req, res) => {
+    let n1 = req.body.A;
+    let n2 = req.body.B;
+    let sum = n1 - n2;
+    res.send(`Результат: ${sum}`);
+});
+
+app.get('/multiply', (req, res) => {
+    let n1 = req.body.A;
+    let n2 = req.body.B;
+    let sum = n1 * n2;
+    res.send(`Результат: ${sum}`);
+});
+
+app.get('/divide', (req, res) => {
+    let n1 = req.body.A;
+    let n2 = req.body.B;
+    let sum = n1 / n2;
+    res.send(`Результат: ${sum}`);
+});
+
+
+app.post('/add', (req, res) =>{
+    let n1 = req.body.A;
+    let n2 = req.body.B;
+    let sum = n1 + n2;
+    console.log(req.body)
+    res.send(`Результат: ${sum}`)
+});
+
+app.post('/subtract', (req, res) =>{
+    let n1 = Number(req.body.A);
+    let n2 = Number(req.body.B);
+    let sum = n1 - n2;
+    console.log(req.body)
+    res.send(`Результат: ${sum}`);
+});
+
+app.post('/multiply', (req, res) =>{
+    let n1 = Number(req.body.A);
+    let n2 = Number(req.body.B);
+    let sum = n1 * n2;
+    console.log(req.body)
+    res.send(`Результат: ${sum}`);
+});
+
+app.post('/divide', (req, res) =>{
+    let n1 = Number(req.body.A);
+    let n2 = Number(req.body.B);
+    let sum = n1 / n2;
+    console.log(req.body)
+    res.send(`Результат: ${sum}`);
+});
+
+
+
+
+
+app.listen(8000);
+
+
